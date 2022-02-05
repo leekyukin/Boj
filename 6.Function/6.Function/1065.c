8c4025889138db8edc5c601f4bd3381e824ad3a0
leekyukin <leekujin14@gmail.com>
2022-02-05 16:37:17 +0900

    2022-02-05 Find numbers with arithmetic sequence

--- 6.Function/6.Function/1065.c
+++ 6.Function/6.Function/1065.c
@@ -7,6 +7,25 @@
 
 #include <stdio.h>
 
+int func(int n);
+
 int main(void) {
-    
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", func(n));
 }
+
+int func(int n) {
+    int cnt = 99;
+    if (n < 100)
+        return n;
+    for(int i = 100; i <= n; i++) {
+        int h = i / 100;
+        int t = (i % 100) / 10;
+        int o = i % 10;
+        if (h - t == t - o)
+            cnt++;
+    }
+    return cnt;
+}
+
